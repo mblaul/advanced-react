2be832f439e102e4ae21548bed1319b58ebbb77c
mblaul <mattblaul@gmail.com>
2019-01-20 22:00:40 -0500

    Coded out backend logic for registration

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,17 +1,21 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
+server.express.use(cookieParser());
+
 server.start(
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
-  },
-  details => {
-    console.log(`Server is now running on http://localhost:${details.port}`);
-  }
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL
+		}
+	},
+	(details) => {
+		console.log(`Server is now running on http://localhost:${details.port}`);
+	}
 );
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		//TODO Check if they are logged in
@@ -38,6 +41,32 @@ const Mutations = {
 		// TODO
 		// Delete the item
 		return ctx.db.mutation.deleteItem({ where }, info);
+	},
+	async signup(parent, args, ctx, info) {
+		// Sanitize email a little bit
+		args.email = args.email.toLowerCase();
+		// Hash password
+		const password = await bcrypt.hash(args.password, 10);
+		// Create the user in the database
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: [ 'USER' ] }
+				}
+			},
+			info
+		);
+
+		// Create the JWT
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// We set the JWT as a cookie on the response
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		return user;
 	}
 };
 
