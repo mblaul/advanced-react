04d62d01ee6662a3d980f712a19ff7964c0e9760
mblaul <mattblaul@gmail.com>
2019-01-25 23:53:47 -0500

    Adding mutation to signup

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -4,6 +4,16 @@ import gql from 'graphql-tag';
 import Form from './styles/Form';
 import queryState from '../lib/queryHelper';
 
+const SIGNUP_MUTATION = gql`
+	mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+		signup(email: $email, name: $name, password: $password) {
+			id
+			email
+			Name
+		}
+	}
+`;
+
 class SignUp extends Component {
 	state = {
 		email: '',
@@ -19,25 +29,31 @@ class SignUp extends Component {
 		const { email, name, password } = this.state;
 
 		return (
-			<Form>
-				<fieldset>
-					<h2>Sign up for an account</h2>
-					<label htmlFor="email">
-						Email
-						<input type="text" name="email" value={email} onChange={this.saveToState} />
-					</label>
-					<label htmlFor="name">
-						Name
-						<input type="text" name="name" value={name} onChange={this.saveToState} />
-					</label>
-					<label htmlFor="password">
-						Password
-						<input type="text" name="password" value={password} onChange={this.saveToState} />
-					</label>
+			<Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
+				{(signup, { error, loading }) => {
+					return (
+						<Form>
+							<fieldset disabled={loading} aria-busy={loading}>
+								<h2>Sign up for an account</h2>
+								<label htmlFor="email">
+									Email
+									<input type="text" name="email" value={email} onChange={this.saveToState} />
+								</label>
+								<label htmlFor="name">
+									Name
+									<input type="text" name="name" value={name} onChange={this.saveToState} />
+								</label>
+								<label htmlFor="password">
+									Password
+									<input type="text" name="password" value={password} onChange={this.saveToState} />
+								</label>
 
-					<button type="submit">Sign Up!</button>
-				</fieldset>
-			</Form>
+								<button type="submit">Sign Up!</button>
+							</fieldset>
+						</Form>
+					);
+				}}
+			</Mutation>
 		);
 	}
 }
