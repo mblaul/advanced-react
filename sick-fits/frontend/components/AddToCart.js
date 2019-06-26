3dec986e815dbf4799f2b87cf0d2b8dc24618e28
mblaul <mattblaul@gmail.com>
2019-06-25 23:04:28 -0400

    Working on cart in side drawer and calculating the total price and individual prices

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
+import { CURRENT_USER_QUERY } from './User';
 import gql from 'graphql-tag';
 
 const ADD_TO_CART_MUTATION = gql`
@@ -15,8 +16,12 @@ class AddToCart extends Component {
   render() {
     const { id } = this.props;
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-        {(addToCart) => <button onClick={addToCart}>Add To Cart</button>}
+      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }} refetchQueries={[ { query: CURRENT_USER_QUERY } ]}>
+        {(addToCart, { loading }) => (
+          <button onClick={addToCart} disabled={loading}>
+            Add To Cart
+          </button>
+        )}
       </Mutation>
     );
   }
